58329d3d01d5d8bdcdce76201365cba69fdeccbb
dmstjd1024 <dmstjd1024@naver.com>
2021-09-26 19:25:48 +0900

    modularization store

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,8 +1,19 @@
 <template>
     <header>
-        <h1>{{ this.$store.state.headerText }}</h1>
+        <h1>{{ getHeaderText }}</h1>
     </header>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default{
+    computed: {
+       ...mapGetters(['getHeaderText'])
+    }
+}
+</script>
+
 <style scoped>
 h1 {
 color:#2F3852;
